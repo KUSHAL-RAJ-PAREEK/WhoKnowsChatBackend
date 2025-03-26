ffe7837448cc1a7ddf3c90b630f3db6fdc88c8b0
KUSHAL RAJ PAREEK <115624967+KUSHAL-RAJ-PAREEK@users.noreply.github.com>
2025-03-26 10:12:25 +0530

    size increse

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 const app = express();
 const server = http.createServer(app);
-
+app.use(express.json({ limit: '50mb' }))
 app.use(cors());
 app.use(express.json());
 
